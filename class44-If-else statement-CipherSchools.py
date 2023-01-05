fccb62a8b955797f5990285460c9fe51d1deb3a3
Abhay singh <119059189+Abhay-singh26124@users.noreply.github.com>
2023-01-05 13:21:08 +0530

    Update class44-If-else statement-CipherSchools.py
    UPDATE

--- class44-If-else statement-CipherSchools.py
+++ class44-If-else statement-CipherSchools.py
@@ -2,6 +2,6 @@ age= input("Enter your age: ")
 age=int(age)
 if age>=14:
     print("line a")
-    print("You are above")
+    print("You are above YOU CAN PLAY")
 else:
     print("Sorry you can't play")
